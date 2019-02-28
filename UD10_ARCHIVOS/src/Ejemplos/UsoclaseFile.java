e21060b2a60895f53f8d560cacc150841c9af966
Esther Lainez <esthertarazona81@gmail.com>
2019-02-28 18:31:37 +0100

    archivos

--- UD10_ARCHIVOS/src/Ejemplos/UsoclaseFile.java
+++ UD10_ARCHIVOS/src/Ejemplos/UsoclaseFile.java
@@ -3,30 +3,38 @@ import java.util.Scanner;
 import java.io.File;
 
 public class UsoclaseFile {
-//muestra informacion acerca del archivo especificado por el ususario
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner teclado =new Scanner(System.in);
-		System.out.println("EScriba el nombre de archivo o directorio"+
-		String ruta=teclado.nextLine();
 
-	}
-	
-	class DemostracionFile {
-		public void analizarRuta(String ruta) {
-			File nombre = new File (ruta);
-			if (nombre.exists()) {//si existe el nombre, muestra informacion del archivo o directorio
-				System.out.println("Existe "+nombre.getName());
-				System.out.println("Es un archivo "+nombre.isFile());
-				System.out.println("Es un directorio "+nombre.isDirectory());
-				System.out.println("Ruta absoluta "+nombre.isAbsolute());
-				System.out.println("Ultima modificacion "+nombre.lastModified());
-				System.out.println("Tamaño "+nombre.length());
-				System.out.println("Ruta "+nombre.getPath());
-				System.out.println("Ruta absoluta"+nombre.getAbsolutePath());
-				System.out.println("Padre "+nombre.getParent());
-			}
-		}
-	}
-
+		public static void main(String[] args) {
+			Scanner teclado = new Scanner( System.in );
+			 System.out.print( "Escriba aqui el nombre del archivo o directorio: " );
+			  String ruta=teclado.nextLine();
+			  File nombre = new File( ruta );
+			 	if (nombre.exists()) {// si existe el nombre, muestra información sobre él
+			 		// muestra información del archivo (o directorio)
+			 		System.out.println("Existe "+nombre.getName());	
+			 		System.out.println("Es un archivo: "+nombre.isFile()) ;	
+			 		System.out.println("es un directorio: "+nombre.isDirectory()); 
+			 		System.out.println("Ruta absoluta: "+nombre.isAbsolute());
+			 		System.out.println("Ultima modificacion: "+	 nombre.lastModified());
+			 		System.out.println( "Tamanio: "+ nombre.length());
+			 		System.out.println( "Ruta: "+ nombre.getPath());
+			 	    System.out.println(  "Ruta absoluta: "+	 nombre.getAbsolutePath()); 
+			 	    System.out.println( "Padre: "+ nombre.getParent());
+		
+		 
+			 	    // muestra el listado del directorio
+			 	    if ( nombre.isDirectory()){
+			 	    		String directorio[] = nombre.list();
+			 	    		System.out.println( "\n\nContenido del directorio:\n" );
+			 	    		for ( String nombreDirectorio : directorio )
+			 	    			System.out.printf( "%s\n", nombreDirectorio );
+			 	    } // fin de else
+			 	 // fin de if exterior
+			 	}else{ 
+			 		// no es archivo o directorio, muestra mensaje de error
+		  			System.out.printf( "%s %s", ruta, "no existe." );
+			 	} // fin de else
+		
+		} // fin de main
+		 // fin de la clase PruebaDemostracionFile
 }
--- UD10_ARCHIVOS/src/Ejemplos/textoSalida.java
+++ UD10_ARCHIVOS/src/Ejemplos/textoSalida.java
@@ -1,34 +1,30 @@
 package Ejemplos;
 
-import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.BufferedWriter;
-import java.io.File;
+import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class textoSalida {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String nombreFichero="C:\\archivos\\prueba3.txt";
+		
 		Scanner teclado=new Scanner (System.in);
-		File f=new File(nombreFichero);
+		System.out.println("Escribe un texto: ");
+		String texto=teclado.nextLine();
 		
 		try {
-			BufferedWriter file = new BufferedWriter (new FileWriter(f));
-		
-			int caracter=file.read();//leemos un caracter
-			while (caracter!=-1) {//mientras no lleguemos al final del archivo
-				System.out.print((char) caracter);
-				caracter=file.read();
-				
-			}
-		
-			file.close();
-		
-		}catch(IOException ex) {
-			System.out.println("El archivo no se puede abrir");
-			
+			BufferedWriter f=new BufferedWriter (new FileWriter(nombreFichero));
+			f.write("ESTE ES EL TEXTO INTRODUCIDO "+texto);
+			f.close();
+		}catch (IOException e) {
+			System.out.println("El fichero no se pudo abrir");
 		}
+		
 	}
+		
+	
 
 }
