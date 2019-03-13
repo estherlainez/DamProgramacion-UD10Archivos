01d454e6b6b0551494cce9b3423eb379d96e9dcb
Esther Lainez <esthertarazona81@gmail.com>
2019-03-13 20:50:03 +0100

    cambio

--- UD10_ARCHIVOS/src/Ejemplos/textoEntrada2.java
+++ UD10_ARCHIVOS/src/Ejemplos/textoEntrada2.java
@@ -1,9 +1,29 @@
 package Ejemplos;
-
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 public class textoEntrada2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String nombreFichero= "c:\\Programación\\ejemplo01.txt";
+		
+		//crear el objeto file
+		File f=new File(nombreFichero);
+		
+		try {
+		BufferedReader file = new BufferedReader (new FileReader(f));
+		String caracter=file.readLine();//leemos un caracter
+		while (caracter!=null) { //mientras no lleguemos al final del archivo
+			System.out.println(caracter);
+			caracter=file.readLine();
+		}
+		file.close();
+		}catch(IOException ex) {
+			System.out.println("el archivo no se puede abrir");
+		}
 
 	}
 
--- UD10_ARCHIVOS/src/Ejemplos/textoSalida2.java
+++ UD10_ARCHIVOS/src/Ejemplos/textoSalida2.java
@@ -1,10 +1,32 @@
 package Ejemplos;
-
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 public class textoSalida2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String nombreFichero="C:\\archivos\\prueba3.txt";
+		String texto="";
+		Scanner teclado=new Scanner (System.in);
+			
+		
+		try {
+			BufferedWriter f=new BufferedWriter (new FileWriter(nombreFichero));
+			
+			do {
+				System.out.println("Escribe un texto");
+				texto=teclado.nextLine();
+				//f.newLine();
+				f.write(texto);
+			}while(texto.compareTo("para")!=0);
+			
+			f.close();
+		}catch (IOException e) {
+			System.out.println("El fichero no se pudo abrir");
+		}
 	}
 
 }
