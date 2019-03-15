b28a2d23984058b0e43d5f5232a52e04deffb232
Esther <esthertarazona81@gmail.com>
2019-03-15 17:43:13 +0100

    ss

--- UD10_ARCHIVOS/src/Ejemplos/EjemploBinario.java
+++ UD10_ARCHIVOS/src/Ejemplos/EjemploBinario.java
@@ -1,6 +1,8 @@
 package Ejemplos;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -8,7 +10,7 @@ public class EjemploBinario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
 		String [] alumnos= {"Andres","Pedro","Juan"};
 		double[]notas= {5.32,1.23,9.87};
 		try {
@@ -24,6 +26,35 @@ public class EjemploBinario {
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
+		
+	//<<<<<AQUI PODEMOS HACER TODAS OPERACIONES QUE QUERAMOS>>>>>>
+		
+		String []alumnosLeidos= new String[3];
+		double[]notasLeidas=new double[3];
+		try {
+			DataInputStream d= new DataInputStream
+					(new FileInputStream("c:\\archivos\\notas.dat"));
+				
+				for(int i=0;i<3;i++) {
+				alumnosLeidos[i]=d.readUTF();
+				notasLeidas[i]=d.readDouble();
+				
+				}
+			d.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(String e:alumnosLeidos) {
+			System.out.println(e);
+		}
+		
+		for(double e:notasLeidas) {
+			System.out.println(e);
+		}
+		
 	}
 
 }
