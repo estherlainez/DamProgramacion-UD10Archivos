e4bf9c3a35dd16fddf056a6983e3090ea605a31c
Esther Lainez <esthertarazona81@gmail.com>
2019-03-17 16:08:01 +0100

    por fin...

--- UD10_ARCHIVOS/src/Ejercicios/Ejercicio6.java
+++ UD10_ARCHIVOS/src/Ejercicios/Ejercicio6.java
@@ -13,22 +13,21 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		int numero=0,contador=0;
+		int numero=0,contador=1;
 		String miNumero="",miCifra="";
 		
 		int numAleatorio= (int)(Math.random()*20);
 		System.out.println("el numero total aleatorio para la cantidad de numeros sera de: "+numAleatorio);
 		
 		int array[]= new int [numAleatorio];
-		int i=numAleatorio-1;
 		
 		System.out.println("Introduzca la ruta de su archivo: ");
 		String ruta= teclado.nextLine();
 		
-		//ruta= "c:\\archivos\\miArchivo.txt";
+		//ruta= c:\\archivos\\miArchivo.txt
 		File miArchivo =new File(ruta);
 		
-		
+	
 		if(miArchivo.exists()) {
 			try {
 				BufferedWriter n=new BufferedWriter (new FileWriter(miArchivo));
@@ -52,13 +51,12 @@ public class Ejercicio6 {
 				BufferedReader l = new BufferedReader (new FileReader(miArchivo));
 				 miCifra=l.readLine();
 				
-				while (miCifra!=null){ 
-					
-					array[i]=Integer.parseInt(miCifra);
-					
-					System.out.println("array  numeros aleatorios-----> "+miCifra);
+				for(int j=0;j<numAleatorio;j++) {
+					array[j]=Integer.parseInt(miCifra);
+					int orden=j+1;
+					System.out.println("array  numeros aleatorios " +orden+"-----> "+miCifra);
 					miCifra=l.readLine();
-					contador++;
+					
 				}
 				l.close();					
 			
@@ -66,11 +64,13 @@ public class Ejercicio6 {
 			catch(IOException ex) {
 					System.out.println("el archivo no se puede abrir");
 			}
-			System.out.println("que hay mal? donde esta el fallo?");
 
 			Arrays.sort(array);
-			
+			System.out.println("Numeros ordenados: ");
 			System.out.println(Arrays.toString(array));
+
+			
 		}
 	}
 }
+
