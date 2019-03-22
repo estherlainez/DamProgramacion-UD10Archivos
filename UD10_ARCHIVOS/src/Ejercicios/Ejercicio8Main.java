4e9b3bd40442f24e7f44f87b9457f401b02359f7
Esther <esthertarazona81@gmail.com>
2019-03-22 17:47:40 +0100

    aqui lo dejo

--- UD10_ARCHIVOS/src/Ejercicios/Ejercicio8Main.java
+++ UD10_ARCHIVOS/src/Ejercicios/Ejercicio8Main.java
@@ -14,45 +14,102 @@ import java.util.Scanner;
 import Ejemplo_Rama_Amigos.Amigo;
 public class Ejercicio8Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner (System.in);
-		int opcion=0,numero=4,limite=10;
 		
-		String [] clientes= {"Esther Lainez","Marcos Moreno","Rosa Jimenez","Estela Magallon"};
+		/*1)Obtener el array de clientes
+		 * si es la 1 vez que entramos al programa haremos como sabemos
+		 * si es la 2 o posterior que entramos al programa lo haremos accediendo
+		 * al archivo
+		 * 
+		 * 2)Desarrollo del programa
+		 * 
+		 * 3)Al finalizar el programa, en la opcion 4, debemos
+		 * escribir el array en el archivo
+		 * 
+		 * desarrollamos pasos 1 y 3
+		 * 
+		 * 1) Si (archivo) existe {significa que ya hemos entrado antesal programa
+		 * 		}else{
+		 * 				Es la primera vez que entramos
+		 * 		}
+		 * 
+		 * 
+		 */
+		
+		
+		int opcion=0,posicion=4,limite=10;
+		
+		String [] clientes= {"Esther Lainez","Marcos Moreno","Rosa Jimenez","Sara Torrellas"};
 		String [] telefonos = {"666555444","654321987","678909876","654321111"};
 		
 		Clientes c1= new Clientes("EstherLainez","666555444");
 		Clientes c2 = new Clientes ("Marcos Moreno","654321987");
 		Clientes c3 = new Clientes ("Rosa Jimenez","678909876");
-		Clientes c4 = new Clientes ("Estela Magallon","654321111");
-		Clientes arrayClientes[]= new Clientes[numero];
+		Clientes c4 = new Clientes ("Sara Torrellas","654321111");
+		//Tengo que crear el array obligatoriamente
+		Clientes arrayClientes[]= new Clientes[10];
+		//tengo que cargar datos a mi array, de donde los voy a sacar?
+		/*
+		 * Si existe el archivo Datos clientes significa que hemos ejecutado
+		 * el programa al menos una vez.
+		 * Por tanto ahi tendremos objetos que podremos cargar
+		 */
 		arrayClientes[0]=c1; arrayClientes[1]=c2; arrayClientes[2]=c3; arrayClientes[3]=c4;
-		
-		
-		
+		Clientes p = null;
+		File file = new File("c:\\archivos\\DatosClientes.dat");
+		int i=0;
 		try {
-			FileOutputStream archivo = new 	FileOutputStream("clientes.dat");
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("clientes.dat"));
-			
-			for (int i=0;i<4;i++) {
-					Clientes cl = new Clientes(clientes[i],telefonos [i]);
-						out.writeObject(cl);
-			} 
-			
-			if (out != null){
-					out.close () ;
-					archivo.close();
-					}
-			out.close();
-			
-		}catch (IOException e) {
-			e.printStackTrace();
+		if(file.exists()) {
+			ObjectInputStream stream= new ObjectInputStream(new FileInputStream(file));
+			while(true) {
+				try {
+					p =(Clientes) stream.readObject();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				arrayClientes[i]=p;
+				i++;
+			}
+		}else {
+			arrayClientes[0]=new Clientes ("Paqui","666555444");
+			arrayClientes[1]=new Clientes ("Pepa","666777987");
+			arrayClientes[2]=new Clientes ("Maria","654321234");
 		}
 		
 		
+		}catch(IOException e) {
+			System.out.println("oooohh");
+		}
+		
+		
+		
+		if(file.exists()) {
+			try {
+				FileOutputStream archivo = new 	FileOutputStream("clientes.dat");
+				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("clientes.dat"));
+			
+				for (int j=0;i<4;j++) {
+						Clientes cl = new Clientes(clientes[i],telefonos [i]);
+							out.writeObject(cl);
+				} 
+			
+				if (out != null){
+						out.close () ;
+						archivo.close();
+						}
+				out.close();
+			
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		
 		try {
-			File file=null;
+			File fi=null;
 			FileInputStream fe = null;
 			ObjectInputStream ois = null;
 			try {
@@ -102,6 +159,8 @@ public class Ejercicio8Main {
 			case 1:
 				System.out.println("Añadir nuevo cliente");
 				
+				
+				System.out.println(arrayClientes[posicion].toString());
 				break;
 			case 2:
 				System.out.println("Modificar datos");
