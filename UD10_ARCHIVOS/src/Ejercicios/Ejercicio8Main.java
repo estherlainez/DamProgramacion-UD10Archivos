8810ff777378829b98da66fade69b97864869558
Esther <esthertarazona81@gmail.com>
2019-03-25 17:40:32 +0100

    me rindoooo

--- UD10_ARCHIVOS/src/Ejercicios/Ejercicio8Main.java
+++ UD10_ARCHIVOS/src/Ejercicios/Ejercicio8Main.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import Ejemplo_Rama_Amigos.Amigo;
 public class Ejercicio8Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner (System.in);
 		
@@ -39,15 +39,9 @@ public class Ejercicio8Main {
 		 */
 		
 		
-		int opcion=0,posicion=4,limite=10;
+		int opcion=0,posicion=0,limite=10;
 		
-		String [] clientes= {"Esther Lainez","Marcos Moreno","Rosa Jimenez","Sara Torrellas"};
-		String [] telefonos = {"666555444","654321987","678909876","654321111"};
-		
-		Clientes c1= new Clientes("EstherLainez","666555444");
-		Clientes c2 = new Clientes ("Marcos Moreno","654321987");
-		Clientes c3 = new Clientes ("Rosa Jimenez","678909876");
-		Clientes c4 = new Clientes ("Sara Torrellas","654321111");
+	
 		//Tengo que crear el array obligatoriamente
 		Clientes arrayClientes[]= new Clientes[10];
 		//tengo que cargar datos a mi array, de donde los voy a sacar?
@@ -56,94 +50,38 @@ public class Ejercicio8Main {
 		 * el programa al menos una vez.
 		 * Por tanto ahi tendremos objetos que podremos cargar
 		 */
-		arrayClientes[0]=c1; arrayClientes[1]=c2; arrayClientes[2]=c3; arrayClientes[3]=c4;
+		
 		Clientes p = null;
 		File file = new File("c:\\archivos\\DatosClientes.dat");
 		int i=0;
-		try {
+		
 		if(file.exists()) {
-			ObjectInputStream stream= new ObjectInputStream(new FileInputStream(file));
-			while(true) {
-				try {
-					p =(Clientes) stream.readObject();
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			try {
+				ObjectInputStream stream= new ObjectInputStream(new FileInputStream(file));
+				while(true) {
+			
+					p =(Clientes) stream.readObject();			
+					arrayClientes[i]=p;
+					i++;
+
 				}
-				arrayClientes[i]=p;
-				i++;
+			}catch(EOFException e) {
+				System.out.println("Fin del fichero. Hemos recuperado "+i);
+			}catch(IOException ex) {
+				System.out.println("Error");
+		
 			}
 		}else {
 			arrayClientes[0]=new Clientes ("Paqui","666555444");
+			posicion++;
 			arrayClientes[1]=new Clientes ("Pepa","666777987");
+			posicion++;
 			arrayClientes[2]=new Clientes ("Maria","654321234");
-		}
-		
-		
-		}catch(IOException e) {
-			System.out.println("oooohh");
-		}
-		
-		
-		
-		if(file.exists()) {
-			try {
-				FileOutputStream archivo = new 	FileOutputStream("clientes.dat");
-				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("clientes.dat"));
+			posicion++;
+			arrayClientes[3]=new Clientes ("Pilar","609878009");
 			
-				for (int j=0;i<4;j++) {
-						Clientes cl = new Clientes(clientes[i],telefonos [i]);
-							out.writeObject(cl);
-				} 
-			
-				if (out != null){
-						out.close () ;
-						archivo.close();
-						}
-				out.close();
-			
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-		
 		}
-		
-		try {
-			File fi=null;
-			FileInputStream fe = null;
-			ObjectInputStream ois = null;
-			try {
-				file = new File("clientes.dat");
-				if (file.exists()){
-					fe = new FileInputStream(file);
-					ois = new ObjectInputStream(fe);
-				while(true) {
-					
-					Clientes cl = null;
-					cl = (Clientes)ois.readObject();
-					cl.print();
-					System.out.println("");
-					}
-				}
-			}catch (EOFException eof) {
-				System.out.println(" --------------------------");
-			}catch (FileNotFoundException fnf) {
-				System.err.println("Fichero no encontrado " + fnf);
-			}catch(IOException e){
-				System.err.println("Se ha producido una IOException");
-					e .printStackTrace();
-			}catch (Throwable e) {
-				System.err.println("Error de programa: " + e);
-					e .printStackTrace() ;
-			}finally{
-				if (ois != 	null) {
-					ois.close ();
-					fe.close () ;
-				}
-			}
-		}catch(IOException e){
-			e .printStackTrace();
-		}
+		//hecho hasta aqui
 		
 		do {
 			System.out.println("MENU");
@@ -156,12 +94,39 @@ public class Ejercicio8Main {
 			opcion=teclado.nextInt();
 			
 			switch(opcion) {
-			case 1:
-				System.out.println("Añadir nuevo cliente");
+			case 1:{
+				System.out.println("¿Que posicion de cliente va a añadir?");
+				int num=teclado.nextInt();
+				teclado.nextLine();
+				System.out.println("Introduzca nombre:");
+				String nombre=teclado.nextLine();
+				System.out.println("Introduzca telefono:");
+				String telefono=teclado.nextLine();
+				/*
+				try {
+					
+					ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("c:\\archivos\\DatosClientes.dat"));
+					Clientes c= new Clientes(nombre,telefono);
+					for (int j=0;i<arrayClientes.length;j++) {	
+								out.writeObject(c);
+					} 
+				
+					
+					
+					if (out != null){
+							out.close () ;	
+					}
 				
 				
-				System.out.println(arrayClientes[posicion].toString());
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+				*/
+				arrayClientes[posicion]=new Clientes(nombre,telefono);
+				posicion++;
+		
 				break;
+			}
 			case 2:
 				System.out.println("Modificar datos");
 				
@@ -176,7 +141,33 @@ public class Ejercicio8Main {
 				
 				break;
 			case 5:
+				
+					try {
+					
+						ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("c:\\archivos\\DatosClientes.dat"));
+					
+						for (int j=0;j<posicion;j++) {	
+							
+									out.writeObject(arrayClientes[j]);
+									System.out.println("posicion de escritura"+j);
+
+						} 
+					
+						System.out.println(arrayClientes[i].toString()+" ");
+						
+						if (out != null){
+								out.close () ;	
+						}
+					
+					
+					}catch (IOException e) {
+						e.printStackTrace();
+					}
+				
+				
+				
 				System.out.println("Hasta pronto!");
+				
 				break;
 			
 			}
